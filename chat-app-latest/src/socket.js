0ab570072c3d87208f38be87c0a92bcc7b186dba
vamsimukesh7 <vamsimukesh2003@gmail.com>
2026-01-28 10:18:01 +0530

    Fix missing config and update socket connection

--- chat-app-latest/src/socket.js
+++ chat-app-latest/src/socket.js
@@ -1,9 +1,10 @@
 import io from "socket.io-client"; // Add this
+import { BASE_URL } from "./config";
 
 let socket;
 
 const connectSocket = (user_id) => {
-  socket = io("https://api.chat.codingmonk.in/", {
+  socket = io(BASE_URL, {
     query: `user_id=${user_id}`,
   });
 } // Add this -- our server will run on port 4000, so we connect to it from here
